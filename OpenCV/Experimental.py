be72ebae8cf35853968a307724d40128dbb27efb
ErfanRasti <64990780+ErfanRasti@users.noreply.github.com>
2021-09-20 13:54:04 +0430

    Delete OpenCV directory

--- OpenCV/Experimental.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-img = cv2.imread("Medias/messi5.jpg")
-grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-template = cv2.imread("Medias/messi_face.jpg", 0)
-h, w = template.shape
-print(template.shape)
